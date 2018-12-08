6719c864d9aa2b483ce28891b838b14fd29be8ec
Vladimir Baranov <baranov-nt@users.noreply.github.com>
2018-12-08 19:03:32 +0500

    0.0.4

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -1,17 +1,9 @@
 <?php
 namespace frontend\controllers;
 
-use Yii;
-use yii\base\InvalidParamException;
-use yii\web\BadRequestHttpException;
+
 use yii\web\Controller;
-use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
-use common\models\LoginForm;
-use frontend\models\PasswordResetRequestForm;
-use frontend\models\ResetPasswordForm;
-use frontend\models\SignupForm;
-use frontend\models\ContactForm;
 
 /**
  * Site controller
